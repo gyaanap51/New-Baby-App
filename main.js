8ecbddec7881cf17fce9934858bf0431b3935a6c
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-11-25 10:53:21 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+objects=[];
 status = "";
 function preload(){
 video = createVideo('video.mp4');
@@ -10,6 +11,37 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 640, 480);
+    if(status !="")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i< objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+ objects.length;
+
+          r=random(255);
+          g=random(255);
+          b=random(255);
+
+
+
+        fill(r, g, b);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke(r, g, b);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
 }
 
 function start()
@@ -22,8 +54,8 @@ function modelLoaded()
     {
  console.log("Model Loaded!");
  status = true;
- //video.loop();//
- video.speed(1.95);
- video.volume(0.7);
+ video.loop();
+ video.speed(1);
+ video.volume(0);
 
     }
